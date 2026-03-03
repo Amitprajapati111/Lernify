ad92c8bd21f3066a6cbe89892d52befe1bc0de4a
amitprajapati111 <amitkumar2020grl@gmail.com>
2026-03-04 02:41:52 +0530

    Update API base URL and adjust Vite build config

--- frontend/src/api/apiClient.js
+++ frontend/src/api/apiClient.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    // baseURL: 'http://localhost:5000/api',
+    baseURL: "https://learnify-backend-1jkh.onrender.com",
 });
 
 // Request interceptor to add auth token
